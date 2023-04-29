7ad9ade7f99f1205f25148cd0e5600f0f05dfb13
ValeriiJavalesson <126103594+ValeriiJavalesson@users.noreply.github.com>
2023-04-29 19:35:43 +0300

    Lesson 14

---
+++ Task_1/Book.java
@@ -0,0 +1,30 @@
+package Task_1;
+
+public class Book implements Comparable<Book>{
+	private int numberOfPages;
+	private String bookName;
+	
+	Book(int numberOfPages, String bookName){
+		this.numberOfPages = numberOfPages;
+		this.bookName = bookName;
+	}
+
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	@Override
+	public int compareTo(Book o) {		
+		if (this.getBookName().compareTo(o.getBookName())==0)
+			return this.getNumberOfPages() - o.getNumberOfPages();
+		else 
+			return this.getBookName().compareTo(o.getBookName());
+	}
+	
+	
+
+}
